2917f8e5d3a5f0edebcb9c74dd6abb7054d2641b
manthan turakhia <manthansturakhia@gmail.com>
2021-05-08 15:00:50 +0530

    creating update api

--- notekeeping/src/components/EditorWindow.js
+++ notekeeping/src/components/EditorWindow.js
@@ -1,21 +1,33 @@
 import { Button, TextField } from '@material-ui/core'
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import "./EditorWindow.css"
-function EditorWindow() {
+import {useStateValue} from "../StateProvider"
+function EditorWindow({editTitle,editBody}) {
     const save = () => {
-        
+            
       };
-    const [title, setTitle] = useState("")
-    const[body,setBody] = useState("")
+    var [title, setTitle] = useState("")
+    var[body,setBody] = useState("")
+      
+    console.log("edittitldoe",editTitle)
+    useEffect(() => {
+       setTitle(editTitle)
+       setBody(editBody)
+       console.log("called")
+    }, [editTitle,editBody])
+    
     return (
         <div className="editorWindow">
-           
+          
             <TextField id="outlined-basic" fullWidth="true" label="Title" variant="outlined"
             style = {{width: 850}} 
-            onChange={(e) => setTitle(e.target.value)}/>
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            />
             <TextField id="outlined-basic" multiline rows={25}
                 fullWidth="true" label="Note" variant="outlined"
             style = {{width: 850}} 
+            value={body}
             onChange={(e) => setBody(e.target.value)}
             />
             <div className="editorWindow__btn">
