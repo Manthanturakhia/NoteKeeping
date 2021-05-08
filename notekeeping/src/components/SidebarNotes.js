ba1b7d4816d70358f2a60a364828a5bf6f6a057e
manthan turakhia <manthansturakhia@gmail.com>
2021-05-08 10:57:22 +0530

    Deleting Note

--- notekeeping/src/components/SidebarNotes.js
+++ notekeeping/src/components/SidebarNotes.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./SidebarNotes.css"
 import DeleteIcon from '@material-ui/icons/Delete';
 import ShareIcon from '@material-ui/icons/Share';
+import { IconButton } from '@material-ui/core';
+import axios from "./axios"
+import {useStateValue} from "../StateProvider"
+import EditIcon from '@material-ui/icons/Edit';
 function SidebarNotes({id,title,body}) {
+    const [{user,noteAdded}, dispatch] = useStateValue()
    var body1 = body.substr(0,22)
    var title1 = title.substr(0,22)
+   const deleteNote = () => {
+            axios
+            .post("/deleteNote", {
+            
+            username: user,
+            id: id,
+            })
+            .then((res) => {
+            console.log(res.data);
+            alert("Note Deleted");
+            dispatch({
+                type: "SET_NOTE_ADDED",
+                noteAdded: true,
+              });
+            })
+            .catch((err) => {
+            console.log(err);
+            alert("Something went wrong");
+            });
+      
+   }
+   const[newBody,setBody] = useState("")
+   const[newTitle,setTitle] = useState("")
+   const editNote = () => {
+        
+   }
     return (
         <div className="sidebarNotes">
            <div className="sidebarNotes__noteData">
@@ -13,8 +44,10 @@ function SidebarNotes({id,title,body}) {
             </div>
             
             <div className="sidebarNotes__icons">
-                 <ShareIcon />
-                 <DeleteIcon />
+            <IconButton onClick={editNote}> <EditIcon /> </IconButton>
+                <IconButton><ShareIcon /> </IconButton>
+                <IconButton onClick={deleteNote}> <DeleteIcon /> </IconButton>
+                
             </div>
         </div>
 
